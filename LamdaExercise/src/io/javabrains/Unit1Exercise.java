bb4d12adc02ff4a5f22f928e3f9685052964d19e
hasya <hasyadurga1304@gmail.com>
2018-07-09 16:43:34 -0500

    project

---
+++ LamdaExercise/src/io/javabrains/Unit1Exercise.java
@@ -0,0 +1,18 @@
+package io.javabrains;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+  List<Person> people = Arrays.asList(new Person("Charles","Dicknes",34),
+		  new Person("Charles","Dicknes",34),
+		  new Person("Charles","Dicknes",34),
+		  new Person("Charles","Dicknes",34));
+		  
+	}
+
+}
